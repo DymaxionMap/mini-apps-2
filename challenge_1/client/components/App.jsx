77155056233ac926c09830ff71be4ab40bd93e08
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-30 10:22:28 -0800

    Implement events search

--- challenge_1/client/components/App.jsx
+++ challenge_1/client/components/App.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      events: [],
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ query: event.target.value });
+  }
+
+  handleClick() {
+    fetch(`/events?q=${this.state.query}`)
+      .then(request => request.json())
+      .then(events => this.setState({ events }));
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        <h1>Historical Events Finder</h1>
+        <label htmlFor="search">
+          Search
+          <input id="search" type="text" onChange={this.handleChange}/>
+        </label>
+        <button type="button" onClick={this.handleClick}>Submit</button>
+        <ul>
+          {this.state.events.map(event => <li key={event.id}>{event.description}</li>)}
+        </ul>
       </div>
     );
   }

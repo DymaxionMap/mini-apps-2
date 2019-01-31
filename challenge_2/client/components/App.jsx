474da229e3119a84b737939aa7718a727581d28b
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-31 11:54:31 -0800

    Render test Chart.js chart

--- challenge_2/client/components/App.jsx
+++ challenge_2/client/components/App.jsx
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Chart from 'chart.js';
+
+const makeChart = (ctx) => {
+  return new Chart(ctx, {
+    type: 'line',
+    data: {
+      datasets: [{
+        data: [1, 2, 3, 4],
+      }],
+    },
+  });
+};
 
 class App extends Component {
   constructor(props) {
@@ -7,9 +19,13 @@ class App extends Component {
     this.state = {
       bpi: {},
     };
+    this.canvas = React.createRef();
   }
 
   componentDidMount() {
+    // TODO: Pass API data to canvas
+    const ctx = this.canvas.current;
+    makeChart(ctx);
     axios.get('/bpi')
       .then((response) => {
         this.setState({ bpi: response.data });
@@ -22,6 +38,7 @@ class App extends Component {
       <div>
         <h1>Cryptocurrency Charter</h1>
         <div>{JSON.stringify(bpi)}</div>
+        <canvas ref={this.canvas} id="chart" width="400" height="300" />
       </div>
     );
   }

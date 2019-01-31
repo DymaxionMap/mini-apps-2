4620565fddd7ecff6d9451a7399bd0180262ae68
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-31 11:18:25 -0800

    Fetch historical Bitcoin data

--- challenge_2/client/components/App.jsx
+++ challenge_2/client/components/App.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bpi: {},
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/bpi')
+      .then((response) => {
+        this.setState({ bpi: response.data });
+      });
+  }
+
   render() {
+    const { bpi } = this.state;
     return (
       <div>
-        Hello World
+        <h1>Cryptocurrency Charter</h1>
+        <div>{JSON.stringify(bpi)}</div>
       </div>
     );
   }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+const apiUrl = 'https://api.coindesk.com/v1/bpi/historical/close.json';
+const startDate = '2018-12-01';
+const endDate = '2019-01-31';
+
+app.get('/bpi', (req, res) => {
+  axios.get(`${apiUrl}?start=${startDate}&end=${endDate}`)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(404);
+    });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));

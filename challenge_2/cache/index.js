13e64f4f03f84e6bf500dc70b091841d40039c2c
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-31 17:31:09 -0800

    Cache search results

--- challenge_2/cache/index.js
+++ challenge_2/cache/index.js
@@ -1,4 +1,7 @@
 const redis = require('redis');
+const bluebird = require('bluebird');
+
+bluebird.promisifyAll(redis);
 
 const client = redis.createClient();
 client.on('error', (err) => {
--- challenge_2/server/controllers.js
+++ challenge_2/server/controllers.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const client = require('../cache');
+
 const { dateToUnixTime, daysBetweenDates, unixTimeToDate } = require('./helpers/convertDate');
 
 const apiUrl = 'https://min-api.cryptocompare.com/data/histoday';
@@ -15,18 +17,32 @@ const formatData = data => ({
 
 const getPrices = (req, res) => {
   const { startDate, endDate, symbol: fromSymbol } = req.query;
-  // const fromSymbol = 'BTC';
-  const endTime = dateToUnixTime(endDate);
-  const numDays = daysBetweenDates(startDate, endDate);
-  axios.get(`${apiUrl}?fsym=${fromSymbol}&tsym=${toSymbol}&toTs=${endTime}&limit=${numDays}`)
-    .then((response) => {
-      const body = response.data;
-      const data = body.Data;
-      res.send(formatData(data));
+  const cacheKey = [fromSymbol, startDate, endDate].join(',');
+  client.getAsync(cacheKey)
+    .then((reply) => {
+      if (reply === null) {
+        const endTime = dateToUnixTime(endDate);
+        const numDays = daysBetweenDates(startDate, endDate);
+        axios.get(`${apiUrl}?fsym=${fromSymbol}&tsym=${toSymbol}&toTs=${endTime}&limit=${numDays}`)
+          .then((response) => {
+            const body = response.data;
+            const data = body.Data;
+            const formattedData = formatData(data);
+            client.set(cacheKey, JSON.stringify(formattedData));
+            res.send(formattedData);
+          })
+          .catch((err) => {
+            console.error(err);
+            res.sendStatus(404);
+          });
+      } else {
+        console.log(reply);
+        res.send(JSON.parse(reply));
+      }
     })
     .catch((err) => {
       console.error(err);
-      res.sendStatus(404);
+      res.sendStatus(500);
     });
 };
 
